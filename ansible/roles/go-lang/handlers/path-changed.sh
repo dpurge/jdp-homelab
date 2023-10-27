c82973ff8ba60d42a0b21af467eac4f7afeb33e5
David P <david.purge@gmail.com>
2023-10-27 21:51:53 +0200

    GoLang installation

--- ansible/roles/go-lang/handlers/path-changed.sh
+++ ansible/roles/go-lang/handlers/path-changed.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-PROFILE=/etc/profile.d/jdp-profile.sh
-PATH_ENTRY=/usr/local/go/bin
-PATH_CMD=path-append
+# PROFILE=/etc/profile.d/jdp-profile.sh
+# PATH_ENTRY=/usr/local/go/bin
+# PATH_CMD=path-append
 
-if [ -d $PATH_ENTRY ] && ! grep --fixed-strings --line-regexp --quiet "$PATH_CMD $PATH_ENTRY" $PROFILE
-then
-    printf "\n# GoLang path\n" >> $PROFILE
-    echo "$PATH_CMD $PATH_ENTRY" >> $PROFILE
-    printf "\n" >> $PROFILE
-fi
+# if [ -d $PATH_ENTRY ] && ! grep --fixed-strings --line-regexp --quiet "$PATH_CMD $PATH_ENTRY" $PROFILE
+# then
+#     printf "\n# GoLang path\n" >> $PROFILE
+#     echo "$PATH_CMD $PATH_ENTRY" >> $PROFILE
+#     printf "\n" >> $PROFILE
+# fi
